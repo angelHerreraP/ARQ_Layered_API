e2ca11dac85902a42ac95e67a63b2f47ce5dd91f
angelherreraP <a.manuel.herrera.p@gmail.com>
2025-08-29 16:17:12 -0600

    First Commit

---
+++ src/main/java/com/exampleLayered/demoLayered/DemoLayeredApplication.java
@@ -0,0 +1,13 @@
+package com.exampleLayered.demoLayered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLayeredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLayeredApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/exampleLayered/demoLayered/model/Usuario.java
@@ -0,0 +1,49 @@
+package com.exampleLayered.demoLayered.model;
+import java.util.*;
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name ="usuarios")//Define el nombre de tu tabla
+@Entity //lo considera una entidad (eso si es considerarlo una tabla)
+@Getter // ahorra escribir
+@Setter// ahorra escribir
+@NoArgsConstructor //genera un constructor vacio
+@AllArgsConstructor //genera un consturctor con tokio y kawazaki
+@Builder //a la hora de construir te ahorra lineas de ocidgo
+public class Usuario {
+
+    //UUID + ID
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(length=100, nullable = false)
+    private String nombre;
+
+    @Column(length=150, nullable = false, unique = true)
+    private String email;
+
+    @Column(name="password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(length=20, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Rol rol = Rol.VENDEDOR;
+
+    @Column(name="fecha_registro", nullable = false, updatable = false)
+    private  LocalDateTime fechaRegistro = LocalDateTime.now();
+
+///modifica este enum para tener mas ROLE
+/// - ADMIN, generales
+/// - VENDEDOR, solo ventas y visualizacion de inventarios
+/// - INGRESOS, edit del INVENTARIO, pero trackeado
+    public enum Rol{
+        ADMIN, VENDEDOR, INGRESOS
+    }
+
+
+
+
+}
